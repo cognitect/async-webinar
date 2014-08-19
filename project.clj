89d7668c759593c8a50ff27760f58043513dcf67
David Nolen <dnolen@cognitect.com>
2014-08-19 15:46:12 -0400

    bump deps

--- project.clj
+++ project.clj
@@ -2,8 +2,9 @@
   :description "FIXME: write this!"
   :url "http://example.com/FIXME"
 
-  :dependencies [[org.clojure/clojure "1.6.0"]
-                 [org.clojure/clojurescript "0.0-2280"]]
+  :dependencies [[org.clojure/clojure "1.7.0-alpha1"]
+                 [org.clojure/clojurescript "0.0-2311"]
+                 [org.clojure/core.async "0.1.338.0-5c5012-alpha"]]
 
   :plugins [[lein-cljsbuild "1.0.4-SNAPSHOT"]]
 
