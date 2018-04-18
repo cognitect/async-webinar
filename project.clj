53788cbd800874aa2ee854a0f01cbc0ddcbb35e8
Jordan Biserkov <biserkov@gmail.com>
2018-04-18 20:29:20 +0300

    bump dependencies

--- project.clj
+++ project.clj
@@ -2,11 +2,11 @@
   :description "FIXME: write this!"
   :url "http://example.com/FIXME"
 
-  :dependencies [[org.clojure/clojure "1.7.0-alpha1"]
-                 [org.clojure/clojurescript "0.0-2311"]
-                 [org.clojure/core.async "0.1.338.0-5c5012-alpha"]]
+  :dependencies [[org.clojure/clojure "1.9.0"]
+                 [org.clojure/clojurescript "1.10.238"]
+                 [org.clojure/core.async "0.4.474"]]
 
-  :plugins [[lein-cljsbuild "1.0.4-SNAPSHOT"]]
+  :plugins [[lein-cljsbuild "1.1.7"]]
 
   :source-paths ["src"]
 
